6bc8eb714e2727fb1e11052dc6f5f50628049bc6
Akash Badshah <akashbad@mit.edu>
2014-04-28 00:46:59 -0400

    added detection of compose box or message insertion

--- Chrome_Extension/securesend.js
+++ Chrome_Extension/securesend.js
@@ -1,2 +1,14 @@
 // test script
-alert("hi");
+
+function insertUI(event) {
+  alert(event);
+}
+
+var insertListener = function(event){
+  if(event.animationName == "composeInserted"){
+    insertUI();
+  }
+}
+
+document.addEventListener("webkitAnimationStart", insertListener, false);
+

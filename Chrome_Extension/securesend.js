9bf0412686c262b0b7a6ccfff80820d338caae3f
Akash Badshah <akashbad@mit.edu>
2014-04-30 12:52:43 -0400

    added UI elements for chrome extension

--- Chrome_Extension/securesend.js
+++ Chrome_Extension/securesend.js
@@ -1,12 +1,48 @@
-// test script
+function encrypt(event){
+  $button = $(event.target);
+}
+
+function decrypt(event){
+  
+}
 
 function insertUI() {
   var composeDivs = $(".n1tfz");
-  //alert(composeDivs);
+  if(composeDivs && composeDivs.length > 0){
+    composeDivs.each(function(){
+      var composeMenu = $(this).parent().parent().parent().parent();
+      if(composeMenu && composeMenu.length > 0 && composeMenu.find(".ss-encrypt-form").length === 0) {
+        var ssEncryptForm = 
+          "<div class='ss-encrypt-form'>" +
+            "<table style='width:100%'>" +
+              "<tbody>" +
+                "<tr>" + 
+                  "<td style='width:80px'>" +
+                    "<div class='ss-encrypt-button'>Send Secure</div>" +
+                  "</td>" +
+                  "<td></td>" +
+                "</tr>" +
+              "</tbody>" +
+            "</table>" +
+          "</div>";
+        composeMenu.append(ssEncryptForm);
+        composeMenu.find(".ss-encrypt-button").click(encrypt); 
+      }
+    });
+  }
+  var messageButtonBar = $("td[class='gH acX']");
+  if(messageButtonBar && messageButtonBar.length > 0) {
+    messageButtonBar.each(function(){
+      if($(this).find(".ss-decrypt-button").length === 0){
+        $(this).prepend("<div class='ss-decrypt-button'>Decrypt</div>");
+        $(this).find(".ss-decrypt-button").click(decrypt);
+      }
+    });
+  }
 }
 
 var insertListener = function(event){
-  if(event.animationName == "composeInserted"){
+  if(event.animationName == "messageOrComposeLoaded"){
     insertUI();
   }
 }
